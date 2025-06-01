0b54e6a29b666608dcb334f194fa4f347850f306
Juliana <jullymary@hotmail.com>
2025-05-31 23:15:49 -0300

    inclusão da classe de configuração para genrenciamento de transações com o mongo db + Springdata

--- springblog/src/main/java/com/fiap/springblog/configuration/MongoTransactionConfig.java
+++ springblog/src/main/java/com/fiap/springblog/configuration/MongoTransactionConfig.java
@@ -1,7 +1,24 @@
 package com.fiap.springblog.configuration;
 
+import com.mongodb.client.MongoClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 public class MongoTransactionConfig {
+
+    //injeção do mongo client
+    @Autowired
+    private MongoClient mongoClient;
+
+    //Faz o gerenciamento das transações em nossos coleções
+    @Bean
+    public MongoTransactionManager transactionManager(MongoDatabaseFactory mongoDatabaseFactory){
+        return new MongoTransactionManager(mongoDatabaseFactory);
+    }
 }
